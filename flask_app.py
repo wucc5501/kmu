f3cb8e1445680d6a0334dccbc68aeb0af7778183
wucc5501 <wucc5501@gmail.com>
2021-05-30 23:47:33 +0800

    20210530

--- flask_app.py
+++ flask_app.py
@@ -1,96 +1,66 @@
 
 import pandas as pd
 import flask
-from test01 import read_data, all_courseID, request_courses, request_courseID, all_courseID, core
+from test01 import read_data, all_courseID, request_courses, request_courseID, all_courseID, core, addno, corebyYear, request_courses,core_pivot
 
-#ctype=set(df['通識類別'])
-#ctype=[c.replace(u'\xa0', u'') for c in ctype]  #去除\xa0
-
+#已開課
 df=read_data('107-109general.xls')
 
-df1=all_courseID(df)
+#所有課程代號
+dfcourse=all_courseID(df)
 
-dfcore=core(df)
+#按年度及核心能力統計核心能力的總和
+dfcore=core_pivot(df)
+#dfcore=core(df)
 
-#courses={}
-
-
-
-
-'''
-dfh=df1[df1['type']=='審美\xa0']
-dfa=df1[(df1['type'] =='基礎\xa0') | (df1['type'] =='進階\xa0') ]  #基礎
-dfb=df1[df1['type']=='體育\xa0']
-dfk=df1[df1['type']=='不分領域\xa0']
-dff=df1[df1['type']=='思考\xa0']
-dfc=df1[df1['type']=='體驗課程\xa0']
-dfe=df1[df1['type']=='全球\xa0']
-dfj=df1[df1['type']=='跨域融通\xa0']
-dfi=df1[df1['type']=='環境科學\xa0']
-dfg=df1[df1['type']=='經典\xa0']
-dfd=df1[df1['type']=='公民\xa0']
-
-
-dfa=dfa.to_dict('records')
-dfb=dfb.to_dict('records')
-dfc=dfc.to_dict('records')
-dfd=dfd.to_dict('records')
-dfe=dfe.to_dict('records')
-dff=dff.to_dict('records')
-dfg=dfg.to_dict('records')
-dfh=dfh.to_dict('records')
-dfi=dfi.to_dict('records')
-dfj=dfj.to_dict('records')
-dfk=dfk.to_dict('records')
-
-def addno(d):
-    n=1
-    for dx in d:
-        dx['no']=n
-        n+=1
-
-    return d
-
-dfa=addno(dfa)
-dfb=addno(dfb)
-dfc=addno(dfc)
-dfd=addno(dfd)
-dfe=addno(dfe)
-dff=addno(dff)
-dfg=addno(dfg)
-dfh=addno(dfh)
-dfi=addno(dfi)
-dfj=addno(dfj)
-dfk=addno(dfk)
-'''
 app = flask.Flask(__name__)
 
 @app.route("/")
 def home():
     return flask.render_template("chart.html")
 
-@app.route("/core")
-def core():
-    return flask.render_template("chartcore.html")
+#核心能力
+@app.route("/coretest")
+def coretest():
+    return flask.render_template("coretest.html")
 
+#查詢年度通識類別的科目
 @app.route('/list', methods=['GET'])
 def listByYearType():
     year=flask.request.args.get('year')
     gtype=flask.request.args.get('gtype')
-    dfx=request_courseID(df1, int(year), gtype)
-    #print(dfx)
+    dfx=request_courseID(dfcourse, int(year), gtype)
+    dfx=addno(dfx)
     html='chart'+str(year)+'.html'
     return flask.render_template(html, courses=dfx.to_dict('records'))
     #return dfx.to_dict()
 
+#顯示年度課程架構圖
 @app.route('/<string:year>', methods=['GET'])
 def listByYear(year):
     html='chart'+year+'.html'
     return flask.render_template(html)
-    
-@app.route("/coremap")
-def k():
-    year=flask.request.args.get('year')
-    return flask.render_template("chartcore.html", corevalues=dfcore.loc[year,'7'])
 
+#顯示年度核心統計圖    
+@app.route("/coremap")
+def coremap():
+    year=int(flask.request.args.get('year'))
+    #return flask.render_template("chartcore.html", corevalues=dfcore.loc[year,'7'])
+    return flask.render_template("chartcore.html", corevalues=corebyYear(dfcore, year), year=year)
+    #return corebyYear(dfcore, year)
+
+#顯示年度科目代碼的課程    
+@app.route("/courselist", methods=['GET'])
+def courselist():
+    year=flask.request.args.get('year')
+    courseID=flask.request.args.get('courseid')
+    dfx=request_courses(df, int(year), courseID)
+    dfx=addno(dfx)
+    return flask.render_template("courselist.html", courses=dfx.to_dict('records'))
+
+
+#課程大綱查詢網址
+#https://wac.kmu.edu.tw/tea/teaaca/team2002c.php?SYEAR=109&SEM=2&SEQNO=9961237
+#核心能力查詢
+#https://wac.kmu.edu.tw/aca/acamap/acara407.php?cond=109,2,9961237
 app.run(debug=True)
--- test01.py
+++ test01.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+#讀取資料
 def read_data(filename):
 
     df=pd.read_excel(filename)
@@ -8,36 +9,51 @@ def read_data(filename):
     columns=df.columns
     columns=[c.strip() for c in columns]
     df.columns=columns
-    #print(df.columns)
+    print(df.columns)
+    #挑選欄位
+    dfx=df[['學年','學期','科目名稱','開課序號','是否開課','科目代碼','選必修','通識類別代碼','通識類別','選課人數','節次起','節次迄','核心能力.1','學習成效權重','學分']]
+    #更換欄位名稱
+    dfx.columns=['學年','學期','科目名稱','開課序號','是否開課','科目代碼','選必修','通識類別代碼','通識類別','選課人數','節次起','節次迄','核心能力','學習成效權重','學分']
+    #篩選已開課
+    dfx=dfx[dfx.是否開課=='Y']
+    return dfx
 
-    return df
-
-#107-109所有科目
+#得所有科目代號，並求出科目代號的開課數量及修課總人數
 def all_courseID(df):
     
-    dfx=df[['學年','科目代碼','科目名稱','選必修','通識類別代碼','通識類別','學分']]
-    dfx=dfx.drop_duplicates(ignore_index=True)
+    dfx=df[['學年','學期','科目代碼','開課序號','科目名稱','選必修','通識類別代碼','通識類別','學分','選課人數']]
+    dfx=dfx.drop_duplicates(subset=['學年','學期','開課序號'], ignore_index=True)
+    #dfx=dfx.sort_values(['學年','科目代碼'],ignore_index=True)
+
+    dfx['開課數量']=dfx.groupby(by=['學年','科目代碼'], sort=False)['科目代碼'].transform('count')
+    dfx['選課總人數']=dfx.groupby(by=['學年','科目代碼'], sort=False)['選課人數'].transform('sum')
+    
+    dfx=dfx.drop_duplicates(subset=['學年','科目代碼'], ignore_index=True)
     return dfx
 #print(df1)
 
-#找出符合學年度及科目代碼的課程
+#找出符合學年度及科目代碼的所有課程
 def request_courses(df, year, courseID):
+    df['科目代碼']=df['科目代碼'].str.strip()
     dfx=df[(df.學年==year) & (df.科目代碼==courseID)]
-    dfx=dfx['學年','科目代碼','科目名稱','選必修','通識類別代碼','通識類別','學分','開課序號']
-    dfx=dfx.sort_values('開課序號',ignore_index=True)
+    #dfx=dfx['學年','學期','科目代碼','科目名稱','選必修','通識類別代碼','通識類別','學分','開課序號']
+    dfx=dfx.drop_duplicates(subset=['學年','學期','開課序號'], ignore_index=True)
+    dfx=dfx.sort_values(['學期','開課序號'],ignore_index=True)
     return dfx
 
 #找出符合學年度及通識類別代碼的課程代碼
 def request_courseID(df, year, generalType):
     dfx=df[(df.學年==year) & (df.通識類別代碼==generalType)]
     dfx=dfx.sort_values('科目代碼',ignore_index=True)
-    dfx.columns=['year','id','name','need','generaltype','generalname','credit']
+    #dfx.columns=['year','sem','id','name','need','generaltype','generalname','credit','courseid']
     return dfx
 
 #找出符合學年度及通識類別代碼的課程代碼及統計開課數及修課人數
 def course_sum(df, year, generalType):
     dfx=df[(df.學年==year) & (df.通識類別代碼==generalType)]
-    dfx=dfx.groupby(by=['學年','通識類別代碼','科目代碼']).count()
+    dfc=[]
+    dfc=dfx.groupby(by=['學年','通識類別代碼','科目代碼']).count()
+    dfs=dfx.groupby(by=['學年','通識類別代碼','科目代碼']).sum()
     #dfx=dfx['學年','科目代碼','科目名稱','選必修','通識類別代碼','通識類別','學分','開課序號']
     print(dfx.columns)
     return dfx
@@ -49,7 +65,7 @@ for index, row in request_courseID(109, '7').iterrows():
     print(index+1, row['name'], row['credit'])
 '''
 
-#依學年度及核心能力為裙組求加權學習成效
+#依學年度及核心能力為群組求加權學習成效
 def core(df):
     dfx=df[['學年','科目名稱','開課序號','科目代碼','選必修','核心能力.1','學習成效權重','學分','通識類別代碼']]
     dfx['核心分數']=dfx['學習成效權重']*0.01*dfx['學分']
@@ -57,33 +73,45 @@ def core(df):
     
     return dfx
 
+def corebyYear(df, year):
+    dfx=df.loc[year,:]
+    coreValues=[]
+    for row in dfx.itertuples():
+        coreValues.append(round(row.total,2))
+    return coreValues
+
 #依學年度及核心能力為index，核心能力為columns，求加權學習成效樞紐分析表  
 def core_pivot(df):
-    dfx=df[['學年','科目名稱','開課序號','科目代碼','選必修','核心能力.1','學習成效權重','學分','通識類別代碼']]
+    dfx=df[['學年','科目名稱','開課序號','科目代碼','選必修','核心能力','學習成效權重','學分','通識類別代碼']]
     dfx['核心分數']=dfx['學習成效權重']*0.01*dfx['學分']
-    dfx=pd.pivot_table(dfx, values='核心分數', index=['學年','核心能力.1'], columns='通識類別代碼', aggfunc='sum', fill_value=0)
-    
+    dfx=pd.pivot_table(dfx, values='核心分數', index=['學年','核心能力'], columns='通識類別代碼', aggfunc='sum', fill_value=0)
+    dfx['total']=dfx.sum(axis=1, skipna=True)
+    dfx['year_core']=dfx.index
+    dfx['year'],dfx['core']=dfx['year_core'].str
+        
     return dfx
-    
+
+def addno(df):
+    dfx=df
+    dfx['no']=dfx.index+1
+
+    return dfx
+
 if __name__=='__main__':
     df=read_data('107-109general.xls')
+    print(df)
 
     df1=all_courseID(df)
     print(df1)
 
-    df2=core(df)
+    df2=request_courses(df,109,'ACHI')
     print(df2)
 
     df3=core_pivot(df)
-    print(sum(df3.loc[107,'A']))
-    '''
-    print(course_sum(109,'7'))
+    print(df3)
 
+    df4=request_courseID(df1, 109, '7')
+    print(df4)
 
-    df2=df[['核心能力.1', '核心能力說明']]
-    df2=df2.drop_duplicates()
-    df2=df2.sort_values('核心能力.1')
-    print(df2)
-
-    print(core(df).loc[107,:])
-    '''
+    df5=corebyYear(df3,109)
+    print(df5)
